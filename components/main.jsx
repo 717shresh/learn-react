976a19e11d066ead0f1372ff28f9ee0593cc3a3d
Shresh <717shreyu@gmail.com>
2025-06-03 15:19:01 +0530

    first commit of react project

---
+++ components/main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <p>
+                hey this is the 2nd project maincontent
+            </p>
+        </main>
+        
+    );
+}
---
+++ components/navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return(
+    <div>
+            <img src="images/react-logo.png" className="nav-logo" alt="React" />
+            <nav >
+                <ul className="nav-list" >
+                    <li className="nav-list-item">Home</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
+    </div>
+
+    )
+}
